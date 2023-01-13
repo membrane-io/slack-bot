e150233e3cf513bf43e6d7bec257afbe38255bd9
Jhonny <aranajhonny@users.noreply.github.com>
2023-01-13 12:32:16 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+// `nodes` contain any nodes you add from the graph (dependencies)
+// `root` is a reference to this program's root node
+// `state` is an object that persist across program updates. Store data here.
+import { nodes, root, state } from "membrane";
+
+export const Root = {
+};
